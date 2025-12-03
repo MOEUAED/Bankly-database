42c7f686077e1a60156d0d98808d6970477fa1d6
MOEUAED <ziyanimouad@gmail.com>
2025-12-03 16:11:55 +0100

    feat (query.sql) : add query (requette) for creat table advisors

--- db.sql
+++ db.sql
@@ -4,12 +4,19 @@ CREATE DATABASE bankly ;
 -- 2. Use database balky ;
 USE bankly ;
 
--- 3. Create first table Customers with cusomer_id(pk)/ full_name/ email/ phone/ registration_date ;
+-- 3. Create first table Customers with customer_id(pk)/ full_name/ email/ phone/ registration_date ;
 CREATE TABLE `customers` ( 
     customer_id INT PRIMARY KEY AUTO_INCREMENT,
     full_name VARCHAR(250) NOT NULL,
     email VARCHAR(250) NOT NULL UNIQUE,
     phone VARCHAR(20) NOT NULL,
     registration_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+-- 4. Create seconde table Advisors with advisor_id(pk)/ full_name/email
+CREATE TABLE `advisors` (
+    advisor_id INT PRIMARY KEY AUTO_INCREMENT,
+    full_name VARCHAR(250) NOT NULL,
+    email VARCHAR(250) NOT NULL UNIQUE
 );
 
