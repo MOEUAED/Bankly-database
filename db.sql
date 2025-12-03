184238056963d90c5154fc23527dc193d24fa38c
MOEUAED <ziyanimouad@gmail.com>
2025-12-03 15:26:06 +0100

    feat (query.sql) : create 2 querys for create and use database bankly and query for create table customers

---
+++ db.sql
@@ -0,0 +1,15 @@
+-- 1. Create database bankly for adding tables ;
+CREATE DATABASE bankly ; 
+
+-- 2. Use database balky ;
+USE bankly ;
+
+-- 3. Create first table Customers with cusomer_id(pk)/ full_name/ email/ phone/ registration_date ;
+CREATE TABLE `customers` ( 
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    full_name VARCHAR(250) NOT NULL,
+    email VARCHAR(250) NOT NULL UNIQUE,
+    phone VARCHAR(20) NOT NULL,
+    registration_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
